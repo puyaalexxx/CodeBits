110461e65f5b7e697af5d6adaf7b3fb56c727a81
Alex <puyaalexxx@gmail.com>
2025-04-18 12:38:15 +0300

    find biggest array number

---
+++ 02-BiggestArrayNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] arrayOfNumbers = {3, 50, 43, 3, 1, 0, 50};
+
+int biggestNumber = arrayOfNumbers[0];
+
+foreach(int number in arrayOfNumbers){
+    if(number > biggestNumber){
+        biggestNumber = number;
+    }
+}
+
+Console.WriteLine(biggestNumber);
