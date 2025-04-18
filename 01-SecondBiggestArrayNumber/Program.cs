b32c170e9eea07f8a1ae39145c731ee24e1e862c
Alex <puyaalexxx@gmail.com>
2025-04-18 12:29:39 +0300

    find the second biggest array number

--- 01-SecondBiggestArrayNumber/Program.cs
+++ 01-SecondBiggestArrayNumber/Program.cs
@@ -1,3 +1,17 @@
-﻿
+﻿int[] arrayOfNumbers = {2, 50, 3, 6, 10, 40, 55, 2, 51};
 
-Console.WriteLine("Hello, World!");
+int biggestNumber = arrayOfNumbers[0];
+int secondBiggestNumber = arrayOfNumbers[0];
+
+foreach(int number in arrayOfNumbers){
+    if(number > biggestNumber){
+        secondBiggestNumber = biggestNumber;
+        biggestNumber = number;
+    }
+    else if(number > secondBiggestNumber && secondBiggestNumber != biggestNumber){
+        secondBiggestNumber = number;
+    }
+}
+
+
+Console.WriteLine(secondBiggestNumber);
